1e18d38c5b9e4b5e5c9fc028a90dacd961ae2a21
Sanket-AI11 <sanketp1104@gmail.com>
2026-01-09 23:11:58 +0530

    Initial admin route

--- shopx/routes/admin.php
+++ shopx/routes/admin.php
@@ -11,7 +11,10 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest')->group(function () {
+Route::middleware('guest')
+->prefix('admin')
+->as('admin.')
+->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
@@ -57,4 +60,3 @@ Route::middleware('auth')->group(function () {
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
-
