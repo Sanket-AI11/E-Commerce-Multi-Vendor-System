86c61385289f82c2ddd87a6c6a78853300d159c8
Sanket-AI11 <sanketpandya7004@gmail.com>
2026-01-26 23:23:51 +0530

    header/footer part

--- shopx/resources/views/frontend/layouts/footer.blade.php
+++ shopx/resources/views/frontend/layouts/footer.blade.php
@@ -81,20 +81,21 @@
                     <div class="widget-about font-md mb-md-3 mb-lg-3 mb-xl-0 wow animate__animated animate__fadeInUp"
                         data-wow-delay="0">
                         <div class="logo mb-30">
-                            <a href="index.html" class="mb-15"><img src="assets/imgs/theme/logo.svg"
+                            <a href="index.html" class="mb-15"><img src="{{ asset('assets/frontend/dist/imgs/theme/logo.svg') }}"
                                     alt="logo" /></a>
                             <p class="font-lg text-heading">Awesome eCommerce store website template</p>
                         </div>
                         <ul class="contact-infor">
                             <li><img src="assets/imgs/theme/icons/icon-location.svg" alt="" /><strong>Address:
-                                </strong> <span>233 North Michigan Avenue, Suite 1800, Chicago, IL 60601</span>
+                                </strong> <span>Atmiya University, Rajkot - 360005</span>
                             </li>
                             <li><img src="assets/imgs/theme/icons/icon-contact.svg" alt="" /><strong>Call
-                                    Us:</strong><span>+9625415 546666464</span></li>
+                                    Us:</strong><span>+91 8320711513, +91 9054741640</span></li>
                             <li><img src="assets/imgs/theme/icons/icon-email-2.svg"
                                     alt="" /><strong>Email:</strong><span>sale@shopx.com</span></li>
                             <li><img src="assets/imgs/theme/icons/icon-clock.svg"
-                                    alt="" /><strong>Hours:</strong><span>10:00 - 18:00, Mon - Sat</span></li>
+                                    alt="" /><strong>Hours:</strong><span>10:00 AM - 6:00 PM, Mon - Sat</span>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -162,26 +163,33 @@
                 <div class="footer-bottom"></div>
             </div>
             <div class="col-xl-4 col-lg-6 col-md-6">
-                <p class="font-sm mb-0">&copy; 2025, <strong class="text-brand">ShopX</strong> - HTML Ecommerce
-                    Template <br />All rights reserved</p>
+                <p class="font-sm mb-0">&copy; 2025, <strong class="text-brand">ShopX</strong><br />All rights
+                    reserved</p>
             </div>
             <div class="col-xl-4 col-lg-6 text-center d-none d-xl-block">
                 <div class="hotline d-lg-inline-flex">
-                    <img src="assets/imgs/theme/icons/phone-call.svg" alt="hotline" />
+                    <img src="{{ asset('assets/frontend/dist/imgs/theme/icons/phone-call.svg') }}" alt="hotline" />
                     <p>0000-000<span>24/7 Support Center</span></p>
                 </div>
             </div>
             <div class="col-xl-4 col-lg-6 col-md-6 text-end d-none d-md-block">
                 <div class="mobile-social-icon">
                     <h6>Follow Us</h6>
-                    <a href="#"><img src="assets/imgs/theme/icons/icon-facebook-white.svg"
+                    <a href="#"><img
+                            src="{{ asset('assets/frontend/dist/imgs/theme/icons/icon-facebook-white.svg') }}"
                             alt="" /></a>
-                    <a href="#"><img src="assets/imgs/theme/icons/icon-twitter-white.svg" alt="" /></a>
-                    <a href="#"><img src="assets/imgs/theme/icons/icon-instagram-white.svg"
+                    <a href="#"><img
+                            src="{{ asset('assets/frontend/dist/imgs/theme/icons/icon-twitter-white.svg') }}"
                             alt="" /></a>
-                    <a href="#"><img src="assets/imgs/theme/icons/icon-pinterest-white.svg"
+                    <a href="#"><img
+                            src="{{ asset('assets/frontend/dist/imgs/theme/icons/icon-instagram-white.svg') }}"
                             alt="" /></a>
-                    <a href="#"><img src="assets/imgs/theme/icons/icon-youtube-white.svg" alt="" /></a>
+                    <a href="#"><img
+                            src="{{ asset('assets/frontend/dist/imgs/theme/icons/icon-pinterest-white.svg') }}"
+                            alt="" /></a>
+                    <a href="#"><img
+                            src="{{ asset('assets/frontend/dist/imgs/theme/icons/icon-youtube-white.svg') }}"
+                            alt="" /></a>
                 </div>
                 <p class="font-sm">Up to 15% discount on your first subscribe</p>
             </div>
