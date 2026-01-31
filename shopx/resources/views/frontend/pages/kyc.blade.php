9e89f70414ae04ba3b7ec33008016d189140b093
Sanket-AI11 <sanketpandya7004@gmail.com>
2026-02-01 00:25:25 +0530

    vendor kyc

---
+++ shopx/resources/views/frontend/pages/kyc.blade.php
@@ -0,0 +1,39 @@
+@extends('frontend.layouts.app')
+
+@section('contents')
+    <x-frontend.breadcrumb :items="[['label' => 'Home', 'url' => '/'], ['label' => 'Login']]" />
+
+    <div class="page-content pt-150 pb-135">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-8 col-lg-10 col-md-12 m-auto">
+                    <div class="row">
+                        <div class="col-lg-6 col-md-8 offset-lg-3">
+                            <x-auth-session-status class="mb-4" :status="session('status')" />
+                            <div class="login_wrap widget-taber-content background-white">
+                                <div class="padding_eight_all bg-white">
+                                    <div class="heading_s1">
+                                        <h4 class="mb-5">KYC Verification</h4>
+                                    </div>
+                                    <form method="post" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group">
+                                            <input type="email" required="" name="email" placeholder="Your Email"
+                                                value="{{ old('email') }}" />
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        </div>
+
+                                        <div class="form-group">
+                                            <button type="submit" class="btn btn-heading btn-block hover-up"
+                                                name="">Submit</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
