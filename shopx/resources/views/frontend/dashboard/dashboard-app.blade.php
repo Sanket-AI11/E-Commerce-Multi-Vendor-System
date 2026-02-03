891bb2be307a03024f2cb3faf2a5522ab1ddcc7d
Sanket-AI11 <sanketpandya7004@gmail.com>
2026-02-03 23:21:18 +0530

    dashboard app

--- shopx/resources/views/frontend/dashboard/dashboard-app.blade.php
+++ shopx/resources/views/frontend/dashboard/dashboard-app.blade.php
@@ -27,7 +27,7 @@
                                         <a class="nav-link"href="#address"><i class="fi-rs-marker mr-10"></i>My Address</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link" href="#account-detail"><i class="fi-rs-user mr-10"></i>Account
+                                        <a class="nav-link" href="{{ route('profile') }}"><i class="fi-rs-user mr-10"></i>Account
                                             details</a>
                                     </li>
                                     <li class="nav-item">
