7f2ce7f6f0855b2b88f93d44ab08e70b315793b2
Sanket-AI11 <sanketpandya7004@gmail.com>
2026-02-03 23:22:38 +0530

    web routes

--- shopx/routes/web.php
+++ shopx/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Frontend\KycController;
 use App\Http\Controllers\Frontend\UserDashboardController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Frontend\ProfileController;
+use App\Http\Controllers\Frontend\VendorDashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +12,21 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('/dashboard', [UserDashboardController::class, 'index'])->name('dashboard');
+
+    // Profile Routes
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+    Route::put('/profile', [ProfileController::class, 'profileUpdate'])->name('profile.update');
+    Route::put('/profile/password', [ProfileController::class, 'passwordUpdate'])->name('password.update');
+
+
+    // KYC Routes
+    Route::get('/kyc-verification', [KycController::class, 'index'])->name('kyc.index');
+    // Route::post('/kyc', [KycController::class, 'kycSubmit'])->name('kyc.submit');
+});
+
+// Vendor Routes
+Route::group(['prefix' => 'vendor', 'as' => 'vendor.', 'middleware' => ['auth', 'verified']], function () {
+    Route::get('/dashboard', [VendorDashboardController::class, 'index'])->name('dashboard');
 });
 
 
