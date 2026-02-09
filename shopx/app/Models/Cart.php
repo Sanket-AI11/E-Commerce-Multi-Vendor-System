f58480fded7cc648cdae0af332b14f51644d86d8
Sanket-AI11 <sanketpandya7004@gmail.com>
2026-02-09 22:41:08 +0530

    cart model

--- shopx/app/Models/Cart.php
+++ shopx/app/Models/Cart.php
@@ -3,19 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Cart extends Model
 {
-
-    function product() : BelongsTo
-    {
-        return $this->belongsTo(Product::class);
-    }
-
-    function variant() : BelongsTo
-    {
-        return $this->belongsTo(ProductVariant::class);
-    }
+    //
 }
