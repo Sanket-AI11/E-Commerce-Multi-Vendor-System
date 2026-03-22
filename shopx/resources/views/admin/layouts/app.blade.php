889ead2d50428080a051ebe77e2ee42c28ba6111
Sanket-AI11 <sanketpandya7004@gmail.com>
2026-03-22 23:15:04 +0530

    admin layout app view

--- shopx/resources/views/admin/layouts/app.blade.php
+++ shopx/resources/views/admin/layouts/app.blade.php
@@ -5,205 +5,45 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Dashboard</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/frontend/dist/imgs/theme/favicon.svg') }}" />
     <!-- BEGIN PAGE LEVEL STYLES -->
     {{-- <link href="./dist/libs/jsvectormap/dist/jsvectormap.css?1750026893" rel="stylesheet" /> --}}
     <!-- END PAGE LEVEL STYLES -->
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link rel="stylesheet" href="{{ asset('assets/global/upload-preview/upload-preview.css') }}" />
-
-    <link href="{{ asset('./assets/admin/css/tabler.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@3.34.0/dist/tabler-icons.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.css">
+    <link href="{{ asset('assets/admin/dist/css/tabler.css') }}" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <!-- END GLOBAL MANDATORY STYLES -->
-    <!-- BEGIN PLUGINS STYLES -->
-    {{-- <link href="./dist/css/tabler-flags.css?1750026893" rel="stylesheet" />
-    <link href="./dist/css/tabler-socials.css?1750026893" rel="stylesheet" />
-    <link href="./dist/css/tabler-payments.css?1750026893" rel="stylesheet" />
-    <link href="./dist/css/tabler-vendors.css?1750026893" rel="stylesheet" />
-    <link href="./dist/css/tabler-marketing.css?1750026893" rel="stylesheet" />
-    <link href="./dist/css/tabler-themes.css?1750026893" rel="stylesheet" /> --}}
-    <!-- END PLUGINS STYLES -->
-    <!-- BEGIN DEMO STYLES -->
-    {{-- <link href="./preview/css/demo.css?1750026893" rel="stylesheet" /> --}}
-    <!-- END DEMO STYLES -->
+
     <!-- BEGIN CUSTOM FONT -->
     <style>
         @import url("https://rsms.me/inter/inter.css");
     </style>
     <!-- END CUSTOM FONT -->
+
     @stack('styles')
 </head>
 
 <body>
     <!-- BEGIN GLOBAL THEME SCRIPT -->
-    <script src="{{ asset('./assets/admin/js/tabler-theme.min.js') }}"></script>
+    <script src="{{ asset('assets/admin/dist/js/tabler-theme.min.js') }}"></script>
     <!-- END GLOBAL THEME SCRIPT -->
     <div class="page">
         <!--  BEGIN SIDEBAR  -->
         @include('admin.layouts.sidebar')
         <!--  END SIDEBAR  -->
         <div class="page-wrapper">
-
-            <!-- BEGIN PAGE HEADER -->
-            {{-- <div class="page-header d-print-none" aria-label="Page header">
-                <div class="container-xl">
-                    <div class="row g-2 align-items-center">
-                        <div class="col">
-                            <!-- Page pre-title -->
-                            <div class="page-pretitle">Overview</div>
-                            <h2 class="page-title">Vertical layout</h2>
-                        </div>
-                        <!-- Page title actions -->
-                        <div class="col-auto ms-auto d-print-none">
-                            <div class="btn-list">
-                                <span class="d-none d-sm-inline">
-                                    <a href="#" class="btn btn-1"> New view </a>
-                                </span>
-                                <a href="#" class="btn btn-primary btn-5 d-none d-sm-inline-block"
-                                    data-bs-toggle="modal" data-bs-target="#modal-report">
-                                    <!-- Download SVG icon from http://tabler.io/icons/icon/plus -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round" class="icon icon-2">
-                                        <path d="M12 5l0 14" />
-                                        <path d="M5 12l14 0" />
-                                    </svg>
-                                    Create new report
-                                </a>
-                                <a href="#" class="btn btn-primary btn-6 d-sm-none btn-icon"
-                                    data-bs-toggle="modal" data-bs-target="#modal-report"
-                                    aria-label="Create new report">
-                                    <!-- Download SVG icon from http://tabler.io/icons/icon/plus -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                        stroke-linecap="round" stroke-linejoin="round" class="icon icon-2">
-                                        <path d="M12 5l0 14" />
-                                        <path d="M5 12l14 0" />
-                                    </svg>
-                                </a>
-                            </div>
-                            <!-- BEGIN MODAL -->
-                            <!-- END MODAL -->
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
-            <!-- END PAGE HEADER -->
-
             <!-- BEGIN PAGE BODY -->
             <div class="page-body">
-                {{-- <div class="container-xl">
-                    <div class="row row-deck row-cards">
-
-                        <div class="col-12">
-                            <div class="row row-cards">
-                                <div class="col-sm-6 col-lg-3">
-                                    <div class="card card-sm">
-                                        <div class="card-body">
-                                            <div class="row align-items-center">
-                                                <div class="col-auto">
-                                                    <span
-                                                        class="bg-primary text-white avatar"><!-- Download SVG icon from http://tabler.io/icons/icon/currency-dollar -->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                            height="24" viewBox="0 0 24 24" fill="none"
-                                                            stroke="currentColor" stroke-width="2"
-                                                            stroke-linecap="round" stroke-linejoin="round"
-                                                            class="icon icon-1">
-                                                            <path
-                                                                d="M16.7 8a3 3 0 0 0 -2.7 -2h-4a3 3 0 0 0 0 6h4a3 3 0 0 1 0 6h-4a3 3 0 0 1 -2.7 -2" />
-                                                            <path d="M12 3v3m0 12v3" />
-                                                        </svg></span>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="font-weight-medium">132 Sales</div>
-                                                    <div class="text-secondary">12 waiting payments</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6 col-lg-3">
-                                    <div class="card card-sm">
-                                        <div class="card-body">
-                                            <div class="row align-items-center">
-                                                <div class="col-auto">
-                                                    <span
-                                                        class="bg-green text-white avatar"><!-- Download SVG icon from http://tabler.io/icons/icon/shopping-cart -->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                            height="24" viewBox="0 0 24 24" fill="none"
-                                                            stroke="currentColor" stroke-width="2"
-                                                            stroke-linecap="round" stroke-linejoin="round"
-                                                            class="icon icon-1">
-                                                            <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                                                            <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                                                            <path d="M17 17h-11v-14h-2" />
-                                                            <path d="M6 5l14 1l-1 7h-13" />
-                                                        </svg></span>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="font-weight-medium">78 Orders</div>
-                                                    <div class="text-secondary">32 shipped</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6 col-lg-3">
-                                    <div class="card card-sm">
-                                        <div class="card-body">
-                                            <div class="row align-items-center">
-                                                <div class="col-auto">
-                                                    <span
-                                                        class="bg-x text-white avatar"><!-- Download SVG icon from http://tabler.io/icons/icon/brand-x -->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                            height="24" viewBox="0 0 24 24" fill="none"
-                                                            stroke="currentColor" stroke-width="2"
-                                                            stroke-linecap="round" stroke-linejoin="round"
-                                                            class="icon icon-1">
-                                                            <path d="M4 4l11.733 16h4.267l-11.733 -16z" />
-                                                            <path d="M4 20l6.768 -6.768m2.46 -2.46l6.772 -6.772" />
-                                                        </svg></span>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="font-weight-medium">623 Shares</div>
-                                                    <div class="text-secondary">16 today</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6 col-lg-3">
-                                    <div class="card card-sm">
-                                        <div class="card-body">
-                                            <div class="row align-items-center">
-                                                <div class="col-auto">
-                                                    <span
-                                                        class="bg-facebook text-white avatar"><!-- Download SVG icon from http://tabler.io/icons/icon/brand-facebook -->
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                            height="24" viewBox="0 0 24 24" fill="none"
-                                                            stroke="currentColor" stroke-width="2"
-                                                            stroke-linecap="round" stroke-linejoin="round"
-                                                            class="icon icon-1">
-                                                            <path
-                                                                d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
-                                                        </svg></span>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="font-weight-medium">132 Likes</div>
-                                                    <div class="text-secondary">21 today</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div> --}}
                 @yield('contents')
             </div>
             <!-- END PAGE BODY -->
-
             <!--  BEGIN FOOTER  -->
             <footer class="footer footer-transparent d-print-none">
                 <div class="container-xl">
@@ -213,10 +53,11 @@
                             <ul class="list-inline list-inline-dots mb-0">
                                 <li class="list-inline-item">
                                     Copyright &copy; 2025
-                                    <a href="." class="link-secondary">ShopX</a>. All rights reserved.
+                                    <a href="https://www.shakilahmeed.com" class="link-secondary">ShopX</a>. All
+                                    rights reserved.
                                 </li>
                                 <li class="list-inline-item">
-                                    <a href="javascrip:void(0)" class="link-secondary" rel="noopener"> v1.0.0 </a>
+                                    <a href="javascript:void(0)" class="link-secondary" rel="noopener"> v1.0.0 </a>
                                 </li>
                             </ul>
                         </div>
@@ -228,12 +69,20 @@
     </div>
 
     <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="{{ asset('assets/global/upload-preview/upload-preview.min.js') }}"></script>
-    <script src="{{ asset('assets/admin/js/tabler.min.js') }}" defer></script>
+    <script src="{{ asset('assets/admin/dist/js/tinymce/tinymce.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/nestable2/1.6.0/jquery.nestable.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="{{ asset('assets/admin/dist/libs/litepicker/dist/litepicker.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <script src="{{ asset('assets/admin/dist/js/tabler.min.js') }}" defer></script>
+    @include('admin.layouts.scripts')
     @stack('scripts')
     <!-- END GLOBAL MANDATORY SCRIPTS -->
-
 </body>
 
 </html>
