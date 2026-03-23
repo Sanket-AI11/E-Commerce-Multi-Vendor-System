98bff94d49c35660a2be76cc6c0f59830733913d
Sanket-AI11 <sanketpandya7004@gmail.com>
2026-03-23 22:42:38 +0530

    register view file for user/vendor

--- shopx/resources/views/auth/register.blade.php
+++ shopx/resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
 @section('contents')
     <x-frontend.breadcrumb :items="[['label' => 'Home', 'url' => '/'], ['label' => 'Register']]" />
 
-    <div class="page-content pt-50 pb-140">
+    <div class="page-content pt-150 pb-140">
         <div class="container">
             <div class="row">
                 <div class="col-12 m-auto">
@@ -13,16 +13,21 @@
                                 <div class="padding_eight_all bg-white">
                                     <div class="heading_s1">
                                         <h2 class="mb-5">Create an Account</h2>
-                                        <p class="mb-30">Already have an account? <a href="{{ route('login') }}">Login</a></p>
+                                        <p class="mb-30">Already have an account? <a href="{{ route('login') }}">Login</a>
+                                        </p>
                                     </div>
                                     <form method="POST" action="{{ route('register') }}">
                                         @csrf
+
                                         <div class="form-group">
-                                            <input type="text" required="" name="name" placeholder="Username" value="{{ old('name') }}"/>
+                                            <input type="text" required="" name="name" placeholder="Name"
+                                                value="{{ old('name') }}" />
                                             <x-input-error :messages="$errors->get('name')" class="mt-2" />
                                         </div>
+
                                         <div class="form-group">
-                                            <input type="email" required="" name="email" placeholder="Email" value="{{ old('email') }}" />
+                                            <input type="email" required="" name="email" placeholder="Email"
+                                                value="{{ old('email') }}" />
                                             <x-input-error :messages="$errors->get('email')" class="mt-2" />
                                         </div>
                                         <div class="form-group">
@@ -38,14 +43,14 @@
                                         <div class="payment_option mb-30">
                                             <div class="custome-radio">
                                                 <input class="form-check-input" required="" type="radio"
-                                                    name="user_type" id="exampleRadios3" value="user" checked="" />
+                                                    name="user_type" id="exampleRadios3" checked="" value="user" />
                                                 <label class="form-check-label" for="exampleRadios3"
                                                     data-bs-toggle="collapse" data-target="#bankTranfer"
                                                     aria-controls="bankTranfer">I am a customer</label>
                                             </div>
                                             <div class="custome-radio">
                                                 <input class="form-check-input" required="" type="radio"
-                                                    name="user_type" id="exampleRadios4" value="vendor" checked="" />
+                                                    name="user_type" id="exampleRadios4" checked="" value="vendor" />
                                                 <label class="form-check-label" for="exampleRadios4"
                                                     data-bs-toggle="collapse" data-target="#checkPayment"
                                                     aria-controls="checkPayment">I am a vendor</label>
@@ -60,6 +65,7 @@
                                 </div>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
