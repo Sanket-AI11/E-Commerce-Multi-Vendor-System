15ae7f6847f9339a9503cc933a5c1375facec63c
Sanket-AI11 <sanketpandya7004@gmail.com>
2026-03-24 22:21:03 +0530

    dashboard account index blade file

--- shopx/resources/views/frontend/dashboard/account/index.blade.php
+++ shopx/resources/views/frontend/dashboard/account/index.blade.php
@@ -1,6 +1,6 @@
 @extends('frontend.dashboard.dashboard-app')
 
-@section('dashboard-contents')
+@section('dashboard_contents')
     <div id="account-detail" role="tabpanel" aria-labelledby="account-detail-tab">
         <div class="card">
             <div class="card-header p-0">
@@ -12,20 +12,25 @@
                     @csrf
                     @method('PUT')
                     <div class="row mt-30">
-                        <x-input-image id="image-preview" name="avatar" :image="{{ auth('web')->user()->avatar }}" />
 
-                        <div class="form-group col-md-10">
+                        <x-input-image imageUploadId="image-upload" imagePreviewId="image-preview" imageLabelId="image-label" name="avatar" :image="auth('web')->user()->avatar" />
+
+                        <div class="form-group col-md-12">
                             <label>Name <span class="required">*</span></label>
                             <input required="" class="form-control" name="name" type="text"
                                 value="{{ auth('web')->user()->name }}" />
                             <x-input-error :messages="$errors->get('name')" class="mt-2" />
                         </div>
-                        <div class="form-group col-md-10">
+
+
+                        <div class="form-group col-md-12">
                             <label>Email Address <span class="required">*</span></label>
                             <input required="" class="form-control" name="email" type="email"
                                 value="{{ auth('web')->user()->email }}" />
                             <x-input-error :messages="$errors->get('email')" class="mt-2" />
+
                         </div>
+
                         <div class="col-md-12">
                             <button type="submit" class="btn btn-fill-out submit font-weight-bold" name="submit"
                                 value="Submit">Save Change</button>
@@ -34,8 +39,7 @@
                 </form>
             </div>
         </div>
-        <hr>
-        {{-- Password section --}}
+
         <div class="card mt-4">
             <div class="card-header p-0">
                 <h5>Change Password</h5>
@@ -46,37 +50,24 @@
                     @csrf
                     @method('PUT')
                     <div class="row mt-30">
-                        <div class="form-group col-md-10">
+                        <div class="form-group col-md-12">
                             <label>Current Password <span class="required">*</span></label>
                             <input required="" class="form-control" name="current_password" type="password" />
                             <x-input-error :messages="$errors->get('current_password')" class="mt-2" />
+
                         </div>
-                        <div class="form-group col-md-10">
+                        <div class="form-group col-md-12">
                             <label>New Password <span class="required">*</span></label>
                             <input required="" class="form-control" name="password" type="password" />
                             <x-input-error :messages="$errors->get('password')" class="mt-2" />
+
                         </div>
-                        <div class="form-group col-md-10">
+                        <div class="form-group col-md-12">
                             <label>Confirm Password <span class="required">*</span></label>
                             <input required="" class="form-control" name="password_confirmation" type="password" />
                             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+
                         </div>
-                        {{-- <div class="form-group col-md-12">
-                            <label>Select Box <span class="required">*</span></label>
-                            <select class="select-active">
-                                <option value="#">656566</option>
-                                <option value="#">656566</option>
-                                <option value="#">656566</option>
-                                <option value="#">656566</option>
-                                <option value="#">656566</option>
-                                <option value="#">656566</option>
-                                <option value="#">656566</option>
-                            </select>
-                        </div>
-                        <div class="form-group col-md-12">
-                            <label>Message <span class="required">*</span></label>
-                            <textarea rows="6" class="form-control" placeholder="Message"></textarea>
-                        </div> --}}
                         <div class="col-md-12">
                             <button type="submit" class="btn btn-fill-out submit font-weight-bold" name="submit"
                                 value="Submit">Save Change</button>
@@ -88,7 +79,7 @@
     </div>
 @endsection
 
-@push('script')
+@push('scripts')
     <script type="text/javascript">
         $(document).ready(function() {
             $.uploadPreview({
